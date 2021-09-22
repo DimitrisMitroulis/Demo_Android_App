1432231f6a86bf442b034aa1c297ea7ad06853f3
DimitrisMitroulis <amfimitrul@gmail.com>
2021-09-22 21:40:25 +0300

    small changes

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,15 +6,23 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    //general tips
+    //Every view must have at least one horizontal and one vertica constraint.
+    //A vertical constraint can be constrained only to another vertical pane.(You can't constrain a left to top)
+    // Each constraint handle can be used to attach only one constraint,but you can create multiple constraints(from diff. widgets)
+    // to the same anchor point.
+    //Tip: constrain all the elements that you want to stick together to the same side(left/right wall)
 
     public Button OkButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_layout);
+        setContentView(R.layout.third_layout);
+        mainVoid();
     }
+    public void mainVoid(){
 
+    }
     //OkButton.
 }
