bb94e3f97b41121ac8bfcec1b16fdc7c62e59ef3
DimitrisMitroulis <amfimitrul@gmail.com>
2021-09-24 21:17:01 +0300

    shows user input

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -28,27 +29,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.third_layout);
 
-        userInput = (EditText) findViewById(R.id.editText);
-        button = (Button) findViewById(R.id.winButton);
-        textView = (TextView) findViewById(R.id.textView);
+        userInput = findViewById(R.id.editText);
+        button = findViewById(R.id.winButton);
+        textView = findViewById(R.id.textView);
+
         textView.setMovementMethod(new ScrollingMovementMethod());
         mainVoid();
 
 
     }
     private void mainVoid(){
-        View.OnClickListener winButtonListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TimesClicked+=1;
-                textView.append("The button got tapped " + TimesClicked + " times " + userInput.getText()+"\n");
-                textView.setVerticalScrollBarEnabled(true);
-                //textView.setText("\n The button got tapped "+ TimesClicked+" times "+userInput.getText());
 
-                //Toast.makeText(this , "ARE YOU READY?",Toast.LENGTH_LONG ).show(); // example for Toast
+        View.OnClickListener winButtonListener = view -> {
 
-                if(TimesClicked == 1)
-                    textView.setText("\n The button got tapped 1 time " + userInput.getText());
+            if (!String.valueOf(userInput.getText()).equals("")) {
+                TimesClicked += 1;
+                textView.append("\n"+userInput.getText());
+                //textView.append("\n" + String.valueOf(userInput.getText()));
+                if (TimesClicked == 1)
+                    textView.setText(String.valueOf(userInput.getText()));
+            }else{
+                Toast toast = Toast.makeText(getApplicationContext(), "Please write something", Toast.LENGTH_SHORT);
+                toast.show();
+
             }
         };
         button.setOnClickListener(winButtonListener);
